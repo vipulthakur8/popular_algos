a3290f8d4382f26ea1c274da9faa8232a37ea36d
Vipul Thakur <vipulthakurofficial@gmail.com>
2023-08-14 17:03:36 +0530

    added time analysis in insetion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 
-# Funtion return two values: first is sorted list and second is the number of steps taken by the 
-# algorithm
+import time
+
+# Funtion return two values: first is sorted list and second time taken by the algorithm
 
 def insertion_sort(arr: list) -> list:
-	no_of_steps = 0  # this is will count the number of steps taken by the algorithm	
+	start_time = time.time() # this is the start time to of the execution
 	
 	for i, v in enumerate(arr):
-		no_of_steps += 1
-
 		j = i	
-		no_of_steps += 1
-
 		while j > 0 and arr[j-1] > arr[j]:
-			no_of_steps += 2
-	
 			arr[j-1], arr[j] = arr[j], arr[j-1]	
-			no_of_steps += 2
-
 			j = j - 1
-			no_of_steps += 1
 
-	return [arr, no_of_steps]
+	end_time = time.time()  # This is the end time of the execution
+	t = end_time - start_time # time taken for whole operation
+	return [arr, t ]
 		
 
 
